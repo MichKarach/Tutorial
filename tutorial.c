cfa4922906a74a6de02d4369d6759f9b3da60589
MichKarach <mixaliskaraxatzis1@gmail.com>
2025-09-27 14:08:32 +0300

    new update

--- tutorial.c
+++ tutorial.c
@@ -1,15 +1,18 @@
- #include "function.h"
- int main(int argc, char* argv[]){
- int variable_a,variable_b,temp;
- variable_a=75;
- variable_b=32;
- for(int i=0;i<4;i++){
- }
- if(variable_a<variable_b){
- variable_b=variable_b-10;
- }else{
- variable_a=variable_a-10;
- }
- }
- temp=max(variable_a,variable_b);
- 
+#include "function.h"
+
+int fact(int n);
+int main(int argc, char* argv[]){
+    int variable_a,variable_b;
+    int final;
+    variable_a=75;
+    variable_b=fact(9);;      
+    for(int i=0;i<4;i++){
+        if(variable_a<variable_b){
+            variable_b=variable_b-10;
+        }else{
+            variable_a=variable_a-10;
+        }
+    }
+    final=max(variable_a,variable_b);
+    return final;
+}
